ba2bcf9aa44c2b5e6e98b4e9ef8a8692112347af
Dante Archer <dantearcher@delta.edu>
2025-06-04 02:18:29 -0400

    Update all pages for mobile view
    I made sure the the menu slides out from the left on mobile and added it to all pages.

--- pages/page-script.js
+++ pages/page-script.js
@@ -1,6 +1,13 @@
 const toggleButton = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('nav-menu');
+const closeButton = document.getElementById('close-menu');
 
 toggleButton.addEventListener('click', () => {
-navMenu.classList.toggle('active');
+    navMenu.classList.toggle('sidebar-open');
+    toggleButton.classList.toggle('hamburger-hidden');
+});
+
+closeButton.addEventListener('click', () => {
+    navMenu.classList.remove('sidebar-open');
+    toggleButton.classList.remove('hamburger-hidden');
 });
